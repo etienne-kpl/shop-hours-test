010c42aacdbfb2e3e0cfde6b766ef8e40b11c818
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-30 18:56:36 +0100

    added validations to slots

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,10 @@
 class Slot < ApplicationRecord
   belongs_to :shop
+
+  validates :weekday,
+            :start_time,
+            :end_time,
+            presence: true
+  validates :weekday, numericality: { only_integer: true, in: 0..6 }
+  validates :end_time, comparison: { greater_than: :start_time }
 end
