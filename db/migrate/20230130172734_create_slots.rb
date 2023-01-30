18a2a0ea0512f50fbecb7f6c5602ef74a0f4e3f8
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-30 18:36:47 +0100

    created the slot model

---
+++ db/migrate/20230130172734_create_slots.rb
@@ -0,0 +1,11 @@
+class CreateSlots < ActiveRecord::Migration[7.0]
+  def change
+    create_table :slots do |t|
+      t.references :shop, null: false, foreign_key: true
+      t.time :start_time
+      t.time :end_time
+
+      t.timestamps
+    end
+  end
+end
