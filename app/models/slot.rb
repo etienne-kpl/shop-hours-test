e5848cd521f622c1df3148f71fd8ddc1c5de1b8e
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-31 11:11:55 +0100

    added a validation to check overlapping time slots

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -7,4 +7,16 @@ class Slot < ApplicationRecord
             presence: true
   validates :weekday, numericality: { only_integer: true, in: 0..6 }
   validates :end_time, comparison: { greater_than: :start_time }
+
+  validate :check_slot_overlap
+
+  private
+
+  def check_slot_overlap
+    other_slots = Slot.where(shop_id:, weekday:)
+    is_overlapping = other_slots.any? do |other_slot|
+      start_time <= other_slot.end_time && end_time >= other_slot.start_time
+    end
+    errors.add(:overlaps_with_other, message: 'cannot overlap with other time slot') if is_overlapping
+  end
 end
