44cc8052919fe1d489ec745e2e7f2e26691d1188
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-31 12:17:18 +0100

    added localization support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'shops#index'
+    resources :shops, only: %i[index show]
+  end
 end
