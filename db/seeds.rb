efe2307152f9cc6c9525f35efe3cb941b4ab11ce
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-01 16:49:40 +0100

    added a language switch

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,13 @@ Shop.create(name: 'Le Collectionist - Verbier')
 Shop.create(name: 'Le Collectionist - Ibiza')
 Shop.create(name: 'Le Collectionist - Barcelone')
 
+p 'Adding slots to each shop...'
+Shop.all.each do |shop|
+  5.times do |x|
+    shop.slots.create(start_time: '10:30', end_time: '15:00', weekday: x + 1)
+    shop.slots.create(start_time: '17:00', end_time: '20:00', weekday: x + 1)
+  end
+  shop.slots.create(start_time: '10:30', end_time: '20:00', weekday: 6)
+end
+
 p 'Done!'
