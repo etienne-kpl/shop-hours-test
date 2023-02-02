a460775429a18f801a370c5dd3f6df2af503eb67
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-02 13:25:18 +0100

    updated the holiday model and created the unit test for it

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,8 +1,24 @@
 class Holiday < ApplicationRecord
   belongs_to :shop
   validates :date, presence: true
+  validates :start_time, presence: true, if: :end_time?
   validates :end_time,
             presence: true,
             comparison: { greater_than: :start_time },
             if: :start_time?
+
+  validate :check_holidays
+
+  private
+
+  def check_holidays
+    other_holidays = Holiday.where(shop_id:, date:)
+    # Check if it is already set as closed
+    if other_holidays.any? { |el| el.end_time.nil? && el.start_time.nil? }
+      errors.add(:base, :invalid, message: 'Shop is already defined as closed for this date')
+    # Check for overlapping slots
+    elsif other_holidays.any? { |el| start_time <= el.end_time && end_time >= el.start_time }
+      errors.add(:base, :invalid, message: 'Opening hours cannot overlap with another time slot for this holiday')
+    end
+  end
 end
--- test/models/holiday_test.rb
+++ test/models/holiday_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 class HolidayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @shop = shops(:one)
+  end
+
+  test 'a holiday without a shop should be invalid' do
+    holiday = Holiday.new(date: '2023-03-05')
+    assert_not holiday.valid?
+  end
+
+  test 'a holiday without a date should be invalid' do
+    holiday = Holiday.new(shop: @shop)
+    assert_not holiday.valid?
+  end
+
+  test 'a holiday with a date and shop should be valid' do
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05')
+    assert holiday.valid?
+  end
+
+  test 'a holiday with a start_time but without an end_time should be invalid' do
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', start_time: '10:00')
+    assert holiday.invalid?
+  end
+
+  test 'a holiday with an end_time but without a start_time should be invalid' do
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', end_time: '10:00')
+    assert holiday.invalid?
+  end
+
+  test 'a holiday with a start_time greater than its end_time should be invalid' do
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', start_time: '12:00', end_time: '10:00')
+    assert holiday.invalid?
+  end
+
+  test 'a holiday with a start_time less than its end_time should be valid' do
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', start_time: '10:00', end_time: '15:00')
+    assert holiday.valid?
+  end
+
+  # If the shop is already set as closed
+  test 'a new holiday with start_time and end_time defined on an existing holiday without them should be invalid' do
+    Holiday.create(shop: @shop, date: '2023-03-05')
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', start_time: '10:00', end_time: '15:00')
+    assert holiday.invalid?
+  end
+
+  test 'a holiday overlapping with an existing holiday should be invalid' do
+    Holiday.create(shop: @shop, date: '2023-03-05', start_time: '10:00', end_time: '15:00')
+    holiday = Holiday.new(shop: @shop, date: '2023-03-05', start_time: '09:00', end_time: '10:00')
+    assert holiday.invalid?
+  end
 end
